36814adf326257e5423728da43997da924539cda
salee <gabor.salamon@unicomp.hu>
2013-02-14 18:09:50 +0100

    Import into repo

---
+++ TestProject/src/hu/sali/test/TestClass.java
@@ -0,0 +1,15 @@
+package hu.sali.test;
+
+public class TestClass 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
